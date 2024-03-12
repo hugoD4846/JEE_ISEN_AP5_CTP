a0ccb3d3ba2ce4e81a7df604fc4f1d7ff08a3f73
hdutoit <hdutoit@ineat.fr>
2024-03-12 14:43:52 +0100

    Feat: adding Student and Type Entity

--- lab07-core/src/main/java/lab07/core/entity/Student.java
+++ lab07-core/src/main/java/lab07/core/entity/Student.java
@@ -1,6 +1,55 @@
 package lab07.core.entity;
 
-public class Student extends GenericEntity {
+import jakarta.persistence.*;
 
-//TODO add the fields and JPA annotations
+import java.util.List;
+
+public class Student extends GenericEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String email;
+    private String firstname;
+    private String lastname;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "student")
+    private List<Grade> grades;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Grade> grades) {
+        this.grades = grades;
+    }
 }
--- lab07-core/src/main/java/lab07/core/entity/Type.java
+++ lab07-core/src/main/java/lab07/core/entity/Type.java
@@ -1,7 +1,47 @@
 package lab07.core.entity;
 
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
 public class Type extends GenericEntity{
 
-    //TODO add the fields and JPA annotations
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private int weight;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "type")
+    private List<Grade> grades;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public List<Grade> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<Grade> grades) {
+        this.grades = grades;
+    }
 }
