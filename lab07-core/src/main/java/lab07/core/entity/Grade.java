38f8adf2a86aca05cfbf7a478f5d5690ef2f16af
Baptiste Herbecq <baptiste.herbecq@gmail.com>
2024-03-12 14:41:10 +0100

    Feat: adding Subject and Grade Entity

--- lab07-core/src/main/java/lab07/core/entity/Grade.java
+++ lab07-core/src/main/java/lab07/core/entity/Grade.java
@@ -2,12 +2,80 @@ package lab07.core.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
-import java.time.LocalDate;
+import java.sql.Date;
 
-@JsonIgnoreProperties({"student"})//leave it like this, it will help the JSON serialization
-public class Grade extends GenericEntity{
+@Entity
+@JsonIgnoreProperties({ "student" }) // leave it like this, it will help the JSON serialization
+public class Grade extends GenericEntity {
 
-  //TODO add the fields and JPA annotations
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private Date date;
+
+  private int grade;
+
+  @ManyToOne
+  private Student student;
+
+  @ManyToOne
+  private Subject subject;
+
+  @ManyToOne
+  private Type type;
+
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Date getDate() {
+    return this.date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public int getGrade() {
+    return this.grade;
+  }
+
+  public void setGrade(int grade) {
+    this.grade = grade;
+  }
+
+  public Student getStudent() {
+    return this.student;
+  }
+
+  public void setStudent(Student student) {
+    this.student = student;
+  }
+
+  public Subject getSubject() {
+    return this.subject;
+  }
+
+  public void setSubject(Subject subject) {
+    this.subject = subject;
+  }
+
+  public Type getType() {
+    return this.type;
+  }
+
+  public void setType(Type type) {
+    this.type = type;
+  }
+
 }
