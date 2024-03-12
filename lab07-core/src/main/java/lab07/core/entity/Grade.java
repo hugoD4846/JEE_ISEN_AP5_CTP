21d2d4e1ed11b693103500c35745c4128802d4dc
Baptiste Herbecq <baptiste.herbecq@gmail.com>
2024-03-12 23:01:48 +0100

    Feat: Set Validator And Clean Project

--- lab07-core/src/main/java/lab07/core/entity/Grade.java
+++ lab07-core/src/main/java/lab07/core/entity/Grade.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 @Entity
 @JsonIgnoreProperties({ "student" }) // leave it like this, it will help the JSON serialization
@@ -13,18 +15,20 @@ public class Grade extends GenericEntity {
 
   private LocalDate date;
 
+  @Min(value = 0, message = "Grade must be at least 0, poor student...")
+  @Max(value = 20, message = "Grade must be at most 20")
   private int grade;
 
   @ManyToOne
-  @JoinColumn(name="student_id", nullable=false)
+  @JoinColumn(name = "student_id", nullable = false)
   private Student student;
 
   @ManyToOne
-  @JoinColumn(name="subject_id", nullable=false)
+  @JoinColumn(name = "subject_id", nullable = false)
   private Subject subject;
 
   @ManyToOne
-  @JoinColumn(name="type_id", nullable=false)
+  @JoinColumn(name = "type_id", nullable = false)
   private Type type;
 
   public Grade() {
